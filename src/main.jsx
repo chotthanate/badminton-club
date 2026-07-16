0976ab1a7d6d3fa0aad0020ff5257e7b00abf03e
chotthanate <102606186+chotthanate@users.noreply.github.com>
2026-07-16 20:47:29 +0700

    add LIFF signup flow (#6)
    Co-authored-by: Chotthanate Soontornnitinan <call_center@MacBook-Air-khxng-Chotthanate.local>

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import BadmintonApp from "./BadmintonApp.jsx";
+import LiffSignupApp from "./LiffSignupApp.jsx";
 import "./badminton.css";
+
+const isLiffSignup = new URLSearchParams(window.location.search).get("liff") === "signup";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BadmintonApp />
+    {isLiffSignup ? <LiffSignupApp /> : <BadmintonApp />}
   </React.StrictMode>,
 );
